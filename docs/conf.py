e0a3329ab0c1e5a8b6c978a7da5f0113612a2d80
Pangaea Wealth Technology <technology@pangaea-wealth.com>
2026-05-13 20:35:56 +0100

    Create conf.py for Pangaea Wealth documentation
    Initialize documentation configuration for Pangaea Wealth.

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+project = "Pangaea Wealth Docs"
+author = "Pangaea Wealth"
+release = "1.0"
+
+extensions = [
+    "myst_parser",
+]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+
+html_theme = "alabaster"
+html_title = "Pangaea Wealth Docs"
